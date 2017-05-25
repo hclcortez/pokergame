8bdbf6d738ba9e90a4b2ff2da239b6fd00dd8cda
Hover Cortez Leite <hclcortez@gmail.com>
2017-05-25 17:12:46 -0300

    Algumas alterações no Poker.js

--- app/helpers/Poker.js
+++ app/helpers/Poker.js
@@ -1,28 +1,37 @@
 class Poker {
     
     constructor(){
+        this._teste = function(){
+            console.log('no');
+        }
         throw new Erro("CardShuffle doesn't Instantiated");
     }
 
     static flush(cards){
-        console.log('Return flush');
         return cards.filter((item, index, array) => {
-           return item.suit === array[0].suit;
+           return (item.suit === array[0].suit)
         }).slice(0,5);
     }
 
     static pair(cards){
-        console.log('Return flush');
-        return cards.filter((item, index, array) => {
+        let pair = cards.filter((item, index, array) => {
            return item.value === array[0].value;
         }).slice(0,2);
+        console.log(pair);
+        return pair.concat(this.getSomeCardNotsuit(cards,pair[0].suit,3));
+
     }
 
     static fourOfKind(cards){
-        console.log('Return flush');
         return cards.filter((item, index, array) => {
            return item.value === array[0].value;
         }).slice(0,4);
+    }
+
+    static getSomeCardNotsuit(cards,suit,size){
+        return cards.filter((element) => {
+            return element.suit != suit;
+        }).slice(0,size);
     }
 
 }
